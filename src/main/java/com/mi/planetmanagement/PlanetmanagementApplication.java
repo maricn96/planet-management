5ec0d3c7d6168e86e7abadbe0257a3b8a2adbd81
maricn96 <maricnikola752@gmail.com>
2021-12-08 19:06:35 +0100

    Initial

---
+++ src/main/java/com/mi/planetmanagement/PlanetmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.mi.planetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetmanagementApplication.class, args);
+	}
+
+}
